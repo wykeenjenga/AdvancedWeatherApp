325efd554e7b8ea70e316e69214bf256615dfa81
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2022-04-04 00:27:45 +0300

    Updates

--- app/src/main/java/com/wyksofts/wyykweather/data/WeatherService.kt
+++ app/src/main/java/com/wyksofts/wyykweather/data/WeatherService.kt
@@ -1,5 +1,8 @@
 package com.wyksofts.wyykweather.data
 
+import android.telecom.Call
+import com.wyksofts.wyykweather.model.WeatherResponse
+
 interface WeatherService {
 
     @GET("https://api.openweathermap.org/data/2.5/forecast?")

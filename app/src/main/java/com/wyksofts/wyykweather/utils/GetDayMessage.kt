ed87057283fc1e5fbe69f848cb02120fa3a661dd
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2022-04-09 00:15:12 +0300

    UI Update

---
+++ app/src/main/java/com/wyksofts/wyykweather/utils/GetDayMessage.kt
@@ -0,0 +1,19 @@
+package com.wyksofts.wyykweather.utils
+
+import java.util.*
+
+class GetDayMessage {
+
+    fun showDayMessage():String {
+        val c = Calendar.getInstance()
+        val timeOfDay = c.get(Calendar.HOUR_OF_DAY)
+
+        return when (timeOfDay) {
+            in 0..11 -> "Good Morning"
+            in 12..15 -> "Good Afternoon"
+            in 16..20 -> "Good Evening"
+            in 21..23 -> "Good Night"
+            else -> "Hello"
+        }
+    }
+}
